587536df5dc1b85ed2b65c951ab33d7be4a7cdaa
Charles Schreyer <cwschreyer@gmail.com>
2023-03-14 17:03:32 +1300

    Updated Validation service and added user input service.

---
+++ src/AlstefGroupTechnical.BusinessRules/Services/UserInputService.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace AlstefGroupTechnical.BusinessRules.Services
+{
+    public class UserInputService
+    {
+        public static int GetValidNumberFromUser()
+        {
+            int userInput;
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+
+                NumberValidationResult validationResult = ValidationService.ValidateNumber(input);
+
+                if (validationResult != NumberValidationResult.ValidNumber)
+                {
+                    Console.WriteLine($"Invalid input. {validationResult}. Please enter a valid number.");
+                    continue;
+                }
+
+                if (!int.TryParse(input, out userInput))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+
+                return userInput;
+            }
+        }
+    }
+}
